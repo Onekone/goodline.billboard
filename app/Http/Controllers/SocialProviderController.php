e293b1f1ecd6e06ab48be1df12b99eee09a9c21f
onekone <alexs0929@gmail.com>
2018-06-15 17:06:42 +0700

    vk-attempt4

--- app/Http/Controllers/SocialProviderController.php
+++ app/Http/Controllers/SocialProviderController.php
@@ -14,22 +14,82 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class SocialProviderController extends Controller
 {
+
+
     //
     public function redirectToProvider()
     {
         return Socialite::with('vkontakte')->redirect();
     }
 
+    //
     public function handleProviderCallback()
     {
         $userSocial = Socialite::driver('vkontakte')->user();
         $accessTokenResponseBody = $userSocial->accessTokenResponseBody;
 
-        $user_exists = User::where('email',$accessTokenResponseBody['email'])->first();
+        $userByEmail = User::where('email',$accessTokenResponseBody['email'])->first();
+        $userBySocialID = SocialProvider::where('social_id',$userSocial->id)->first();
 
-        $auth = Auth::id();
-        $authUser = Auth::user();
 
+        $auth = Auth::user();
+
+
+        if ($auth) // if logged in
+        {
+            // connect to vk
+            $auth->verified = 1;
+            $newSignUp = SocialProvider::create([
+                'user_id' => $auth,
+                'social_id' => $userSocial->id,
+                'social_provider' => 0
+            ]);
+
+            return redirect()->route('user',$auth->id);
+        }
+        else
+        {
+            // есть ли привзяка
+            if ($userBySocialID) {
+                // sign in
+                Auth::loginUsingId($userBySocialID->user_id, TRUE);
+                return redirect()->route('user',$userBySocialID->user_id);
+            }
+            else {
+                if ($accessTokenResponseBody['email']) { // есть почта в ВК
+                    if ($userByEmail) { // есть ли аккаунт с такой почтой
+                        return redirect()->route('ad.index');
+                    }
+                    else
+                    {
+                        // sign up
+                        $newUser = User::create([
+                            'name' => $userSocial->name,
+                            'email' => $accessTokenResponseBody['email'],
+                            'password' => '',
+                            'verified' => 1,
+                        ]);
+
+                        $newSignUp = SocialProvider::create([
+                            'user_id' => $newUser->id,
+                            'social_id' => $userSocial->id,
+                            'social_provider' => 0
+                        ]);
+
+                        Auth::loginUsingId($newUser->id, TRUE);
+                        return redirect()->route('user',$newUser->id);
+                    }
+                }
+                else
+                {
+                    // нет почты в ВК
+                    return view('auth.askemail')->withUserToken($userSocial)->withAccessToken($accessTokenResponseBody);
+                }
+
+            }
+        }
+
+/*
         if(!$user_exists) {
             // sign up
             $newUser = User::create([
@@ -57,8 +117,10 @@ class SocialProviderController extends Controller
                     return redirect()->route('user',$user_exists->id);
                 }
                 // else to index
-                else
+                else {
                     return redirect()->route('ad.index');
+                }
+
             }
             else {
             // else
@@ -73,7 +135,7 @@ class SocialProviderController extends Controller
                 return redirect()->route('user',$user_exists->id);
             }
         }
-
+    */
         abort(500);
     }
 }
