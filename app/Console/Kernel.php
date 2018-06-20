0af866907138349aec8c85421affdcfc2e5b9f40
onekone <alexs0929@gmail.com>
2018-06-20 14:00:01 +0700

    deploy-hotfix-3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Ad;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call( function() {
+            Ad::where('TIMESTAMPDIFF(SECOND,created_at,now())','>','15*24*60*60')->delete();
+        })->daily();
     }
 
     /**
