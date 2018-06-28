c32a415cfa461ddc383a65d0427be868bc078bae
onekone <alexs0929@gmail.com>
2018-06-28 16:35:39 +0700

    basic test 7

--- tests/Unit/ProfileControllerTest.php
+++ tests/Unit/ProfileControllerTest.php
@@ -8,6 +8,7 @@
 
 namespace Tests\Feature;
 
+use App\SocialProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Auth;
@@ -52,7 +53,7 @@ class ProfileControllerTest extends TestCase {
         $this->notVerifiedUser->save();
 
         $this->mailVerifyRecord = new \App\EmailVerify;
-        $this->mailVerifyRecord->user_id = $this->notVerifiedUser;
+        $this->mailVerifyRecord->user_id = $this->notVerifiedUser->id;
         $this->mailVerifyRecord->verify_token = str_random(60);
         $this->mailVerifyRecord->save();
 
@@ -92,164 +93,169 @@ class ProfileControllerTest extends TestCase {
 
     public function test_WhileUnauth_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileUnauth_UnknownID_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->get(route('user.clear',-1));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuthUnverified_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->notVerifiedUser)->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuthUnverified_NotSelf_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->notVerifiedUser)->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
-    public function test_WhileAuthUnverified_UnknownID_NukeAds_Success() {
+    public function test_WhileAuthUnverified_UnknownID_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->notVerifiedUser)->get(route('user.clear',-1));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuth_NukeAds_Success() {
         // arrange
-
+        $user = $this->verifiedUser;
         // act
-
+        $response = $this->actingAs($user)->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(200);
     }
     public function test_WhileAuth_NotSelf_NukeAds_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->verifiedUser)->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuth_UnknownID_NukeAds_Success() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->verifiedUser)->get(route('user.clear',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
 
     public function test_WhileUnauth_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileUnauth_UnknownID_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuthUnverified_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->notVerifiedUser;
         // act
-
+        $response = $this->actingAs($user)->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuthUnverified_NotSelf_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->notVerifiedUser)->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
-    public function test_WhileAuthUnverified_UnknownID_NukeUser_Success() {
+    public function test_WhileAuthUnverified_UnknownID_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->verifiedUser;
         // act
-
+        $response = $this->actingAs($user)->get(route('user.destroy',-1));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuth_NukeUser_Success() {
         // arrange
-
+        $user = factory(\App\User::class)->create(['verified'=>1]);
         // act
-
+        $response = $this->actingAs($user)->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertRedirect(route('ad.index'));
     }
     public function test_WhileAuth_NotSelf_NukeUser_RedirectBack() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->verifiedUser)->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuth_UnknownID_NukeUser_Success() {
         // arrange
-
+        $user = $this->otherUser;
         // act
-
+        $response = $this->actingAs($this->verifiedUser)->get(route('user.destroy',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
 
-    public function test_WhileUnauth_UnbindVK() {
+    public function test_WhileUnauth_UnbindVK_Redirect() {
         // arrange
-
+        $user = $this->otherUser;
+        $userBind = SocialProvider::firstOrCreate(['user_id'=>$user->id,'social_id'=>1234567,'social_provider'=>0]);
         // act
-
+        $response = $this->get(route('user.unbindVK',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
     public function test_WhileAuth_Unbound_UnbindVK() {
         // arrange
-
+        $user = $this->otherUser;
+        SocialProvider::where('user_id',$user->id)->delete();
         // act
-
+        $response = $this->actingAs($user)->get(route('user.unbindVK',$user->id));
         // assert
-
+        $response->assertStatus(302);
     }
-    public function test_WhileAuth_Bound_UnbindVK() {
+    public function test_WhileAuth_Bound_UnbindVK_Success() {
         // arrange
-
+        $user = $this->verifiedUser;
+        $userBind = SocialProvider::firstOrCreate(['user_id'=>$user->id,'social_id'=>1234567,'social_provider'=>0]);
+        $userBind->save();
         // act
-
+        $response = $this->actingAs($this->verifiedUser)->get(route('user.unbindVK',$user->id));
         // assert
-
+        $response->assertStatus(200);
     }
 
-    public function test_WhileUnauth_Verify_Failure() {
+    public function test_WhileUnauth_Verify_Success() {
+
     }
     public function test_WhileUnauth_UnknownToken_Verify_Failure() {
     }
-    public function test_WhileAuth_Verify_Failure() {
+    public function test_WhileAuth_Verify_Success() {
     }
     public function test_WhileAuth_UnknownToken_Verify_Failure() {
     }
