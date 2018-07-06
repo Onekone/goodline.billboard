c7ec6cc794e6902ff7e293d73b70f6423e006408
Eduard <edik.skrim@gmail.com>
2018-07-06 12:45:09 +0700

    hotfix

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title','Объявление')
 @section('content')
     <div class="container">
         @if (session('status') && session('status-class'))
--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title','Типа.Билборд')
 @section('content')
 
     <div class="container">
